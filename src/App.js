bc99fd3f394911109fb0e378a59f1503791bdaab
Yayiyi94 <yadiratr94@gmail.com>
2025-09-01 13:41:02 -0600

    Clean archive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Weather App</h1>
       </header>
+      <footer>
+        Coded by Yadira DLTR, code hosted on{" "}
+        <a
+          href="https://github.com/Yayiyi94/weather-app-react-v2"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and website hosted on{" "}
+        <a
+          href="https://weather-react-ydltr.netlify.app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
